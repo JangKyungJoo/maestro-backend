b27ce577cdc4f4cb09518d4b05c25a91fe665b9f
root <root@ip-172-31-26-73.us-west-2.compute.internal>
2016-07-21 02:01:03 +0000

    image7

--- routes/board.js
+++ routes/board.js
@@ -1,6 +1,20 @@
 var express = require('express');
 var mysql =require('mysql');
 var multiparty = require('multiparty');
+var fs = require('fs');
+var multer = require('multer');
+var storage = multer.diskStorage({
+	destination: function(req, file, callback) {
+		callback(null, './uploads');
+	},
+	filename: function(req, file, callback) {
+		var filename = Date.now();
+		callback(null, filename);
+	}
+});
+var upload = multer({
+	storage : storage
+}).single('photo');
 var router = express.Router();
 
 var connection =mysql.createConnection({
@@ -8,6 +22,13 @@ var connection =mysql.createConnection({
     'user' : 'user', 
     'password' : '1q2w3e4r', 
     'database' :'maestro',
+});
+
+// return img
+router.get('/tmp/:path', function(req, res){
+    var img = req.params.path;
+    console.log("path : "+img);
+    res.sendFile("/tmp/"+img);
 });
 
 // show list
@@ -22,12 +43,23 @@ router.get('/:content_id', function(req, res, next) {
     var id = req.params.content_id;
     connection.query('select * from board where id=?;',[id], function (error, cursor){
         if(error == null){    
-            if (cursor.length > 0) 
-                res.json(cursor[0]);
-            else
+            if (cursor.length > 0){
+		if(cursor[0].photoid == 1){
+			connection.query('select path from photo where postid=?;', [id], function(error, cur){
+				if(error==null){
+					res.json({
+						result : true, writerid : cursor[0].writerid, title : cursor[0].title, content : cursor[0].content, photopath : cur[0].path, timestamp : cursor[0].timestamp});
+				}else{			
+					res.status(503).json({result : false, reason : "Cannot find selected article"});
+				}
+			});
+		}else
+                    res.json(cursor[0]);
+            }else{
                 res.status(503).json({ 
                     result : false, reason : "Cannot find selected article"
                 });
+	    }
         }else{
             console.log("err : "+error);
         }
@@ -36,43 +68,63 @@ router.get('/:content_id', function(req, res, next) {
 
 // insert 
 router.post('/', function(req, res, next){
-    console.log("insert board");
     var form = new multiparty.Form();
     form.parse(req, function(err, fields, files){
         var title = fields.title;
         var content = fields.content;
-        var file = files.photo;
-        var len = Object.keys(fields).length;
+	console.log("files : " + files.photo);
+	if(files.photo != undefined)
+            var file = files.photo[0];
+        //var len = Object.keys(fields).length;
         if(file == null){
-            console.log("no file");
-            console.log("title : " + title);
-            console.log("content : "+ content);
+	    connection.query('insert into board(writerid, title, content) values (?, ?, ?);', [1, title, content], function(err, info){
+		if(err==null){
+			connection.query('select * from board where id=?;', [info.insertId], function(err, cursor){
+				if(cursor.length > 0){
+					res.json({
+						result : true, id : cursor[0].id, title : cursor[0].title, timestamp : cursor[0].timestamp
+					});
+				}else{
+					res.status(503).json({  result: false, reason : "Cannot post article"});
+				}
+			});
+		}else{
+			console.log(err);
+			res.status(503).json(err);
+		}
+	    });
         }else{
-            console.log("file : " + file);
-        }
-        /*
-        if(len>2){
-		console.log("len > 3");
-        }else{
-            connection.query('insert into board(writerid, title, content, photoid) values (?, ?, ?, ?);', [1, title, content, null], function (error, info){
-                if (error == null){
-                    connection.query('select * from board where id=?;', [info.insertId], function (error, cursor){
-                        if (cursor.length > 0) { 
-                            res.json({
-                                result : true, id : cursor[0].id, title : cursor[0].title, timestamp :cursor[0].timestamp,
-                            });
-                        }
-                        else
-                            res.status(503).json({ result : false, reason : "Cannot post article"});
-                    });
-                }
-                else{
-    		        console.log(error);
-                    res.status(503).json(error);
-    	        }
-            });
-        }
-        */
+  	    upload(req, res, function(err){
+		if(err)
+			console.log("upload err : "+err);
+		else
+			console.log("upload succes");
+	    });
+	    console.log("file :  "+file.name+", "+file.path+", "+file.size);
+	    connection.query('insert into board(writerid, title, content, photoid) values (?, ?, ?, ?);', [1, title, content, 1], function(err, info){
+		if(err==null){
+			connection.query('insert into photo(postid, path) values (?, ?);', [info.insertId, file.path], function(err, info){
+			if(err==null){
+				connection.query('select * from board where id=?;', [info.insertId], function(err, cursor){
+					if(cursor.length > 0){
+						res.json({
+							result : true, id : cursor[0].id, title : cursor[0].title, timestamp : cursor[0].timestamp, photoid : cursor[0].photoid
+						});
+					}else{
+						res.status(503).json({result : false, reason : "Cannot post article"});
+					}
+				});
+			}else{
+				console.log(err);
+				res.status(503).json(err);
+			}
+			});
+		}else{
+			console.log(err);
+			res.status(503).json(err);
+		}
+	    });
+	}
     });
 });
 
